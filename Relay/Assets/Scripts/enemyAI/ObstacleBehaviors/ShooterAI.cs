ca761133f0e20f2f2b0d4f180ae56733fdc1d65e
Kenjiro Goodson <kcgoodson@gmail.com>
2018-04-07 21:22:54 -0700

    ImportedLemonLimeAndOtherAssets

---
+++ Relay/Assets/Scripts/enemyAI/ObstacleBehaviors/ShooterAI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterAI : Unit {
+
+	public GameObject bullet;
+	public float firingSpeed = 1;
+
+	void LaunchBullet() {
+		GameObject shot = Instantiate (bullet, transform.position, Quaternion.identity);
+		shot.GetComponent<Rigidbody2D> ().velocity = transform.up * firingSpeed;
+	}
+
+}
